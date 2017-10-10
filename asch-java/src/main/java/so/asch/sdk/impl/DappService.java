7414ebe533b7bc7c35e69c1f78ea5549f32e2b86
Cheng Liang <chengliang@meishubao.com>
2017-10-10 15:49:48 +0800

    Dapp impemention define

--- asch-java/src/main/java/so/asch/sdk/impl/DappService.java
+++ asch-java/src/main/java/so/asch/sdk/impl/DappService.java
@@ -1,6 +1,68 @@
 package so.asch.sdk.impl;
 
+import so.asch.sdk.AschResult;
 import so.asch.sdk.Dapp;
+import so.asch.sdk.dto.query.BlockQueryParameters;
+import so.asch.sdk.dto.query.TransactionQueryParameters;
 
 public class DappService extends AschRESTService implements Dapp {
+    @Override
+    public AschResult getBlockHeight(String dappID) {
+        return null;
+    }
+
+    @Override
+    public AschResult queryBlocks(BlockQueryParameters parameters) {
+        return null;
+    }
+
+    @Override
+    public AschResult getAccount(String address) {
+        return null;
+    }
+
+    @Override
+    public AschResult deposit(String dappID, String currency, long amount, String message, String secret, String secondSecret) {
+        return null;
+    }
+
+    @Override
+    public AschResult withdraw(String dappID, String currency, long amount, String message, String secret, String secondSecret) {
+        return null;
+    }
+
+    @Override
+    public AschResult innerTransfer(String dappID, String currency, String recipientId, long amount, String message, String secret, String secondSecret) {
+        return null;
+    }
+
+    @Override
+    public AschResult setNickname(String dappID, String nickname) {
+        return null;
+    }
+
+    @Override
+    public AschResult queryUnconfirmedTransactions(String dappID) {
+        return null;
+    }
+
+    @Override
+    public AschResult queryTransactions(TransactionQueryParameters parameters) {
+        return null;
+    }
+
+    @Override
+    public AschResult getTransaction(String transactionId) {
+        return null;
+    }
+
+    @Override
+    public AschResult getTransactions(String ownerId, String currency, int limit, int offset) {
+        return null;
+    }
+
+    @Override
+    public AschResult getContracts() {
+        return null;
+    }
 }
