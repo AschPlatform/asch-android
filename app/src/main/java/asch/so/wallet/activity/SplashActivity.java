dbd63814f633db8df7ddf9d3ff1ca4dde8f8c2b2
Cheng Liang <chengliang@meishubao.com>
2017-10-13 14:39:34 +0800

    add a particle splash view

---
+++ app/src/main/java/asch/so/wallet/activity/SplashActivity.java
@@ -0,0 +1,47 @@
+package asch.so.wallet.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import asch.so.base.activity.BaseActivity;
+import asch.so.wallet.R;
+import asch.so.widget.particleview.ParticleView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by kimziv on 2017/10/13.
+ */
+
+public class SplashActivity extends BaseActivity {
+
+
+    @BindView(R.id.splash_pv)
+    ParticleView particleView;
+
+    private Unbinder unbinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        unbinder=ButterKnife.bind(this);
+        particleView.startAnim();
+        particleView.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
+                Intent intent = new Intent(SplashActivity.this, MainTabActivity.class);
+                SplashActivity.this.startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+}
