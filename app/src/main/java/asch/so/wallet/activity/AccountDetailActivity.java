97518466a427cb697947b4b4f0f086e416c925cc
Cheng Liang <chengliang@meishubao.com>
2017-10-27 16:23:45 +0800

    input dialog

--- app/src/main/java/asch/so/wallet/activity/AccountDetailActivity.java
+++ app/src/main/java/asch/so/wallet/activity/AccountDetailActivity.java
@@ -3,10 +3,16 @@ package asch.so.wallet.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.OnClickListener;
+import com.orhanobut.dialogplus.ViewHolder;
 
 import asch.so.base.activity.BaseActivity;
 import asch.so.base.util.ActivityUtils;
@@ -22,15 +28,22 @@ import butterknife.ButterKnife;
  * Created by kimziv on 2017/10/27.
  */
 
-public class AccountDetailActivity extends BaseActivity {
+public class AccountDetailActivity extends BaseActivity implements View.OnClickListener{
 
     @BindView(R.id.toolbar)
     TitleToolbar toolbar;
+    @BindView(R.id.backup_btn)
+    Button backupBtn;
+    @BindView(R.id.delete_btn)
+    Button deleteBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_account_detail);
         ButterKnife.bind(this);
+        backupBtn.setOnClickListener(this);
+        deleteBtn.setOnClickListener(this);
        // AccountDetailFragment fragment = AccountDetailFragment.newInstance();
         //ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),fragment, R.id.fragment_container);
         StatusBarUtil.immersive(this);
@@ -51,6 +64,38 @@ public class AccountDetailActivity extends BaseActivity {
             }
         });
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view==backupBtn){
+
+        }else if(view==deleteBtn){
+            DialogPlus dialog = DialogPlus.newDialog(this)
+                    .setContentHolder(new ViewHolder(R.layout.dialog_input_passwd))
+                    .setGravity(Gravity.BOTTOM)
+                    .setCancelable(false)
+                    .setExpanded(true)  // This will enable the expand feature, (similar to android L share dialog)
+                    .setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(DialogPlus dialogPlus, View view) {
+                            switch (view.getId()){
+                                case R.id.ok_btn:
+                                {
+                                    dialogPlus.dismiss();
+                                }
+                                    break;
+                                case R.id.cancel_btn:
+                                {
+                                    dialogPlus.dismiss();
+                                }
+                                break;
+                            }
+                        }
+                    })
+                    .create();
+            dialog.show();
+        }
     }
 
     @Override
