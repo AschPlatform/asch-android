2f61c200a285890f33020a5f70526ef350068321
kimziv <kimziv@qq.com>
2017-11-10 14:41:57 +0800

    add + - for transaction item

--- app/src/main/java/asch/so/wallet/view/adapter/AssetTransactionsAdapter.java
+++ app/src/main/java/asch/so/wallet/view/adapter/AssetTransactionsAdapter.java
@@ -14,6 +14,8 @@ import java.util.List;
 
 import asch.so.base.adapter.BaseRecyclerViewAdapter;
 import asch.so.wallet.R;
+import asch.so.wallet.accounts.AccountsManager;
+import asch.so.wallet.model.entity.Account;
 import asch.so.wallet.model.entity.Transaction;
 import asch.so.wallet.model.entity.UIATransferAsset;
 import butterknife.BindView;
@@ -35,15 +37,20 @@ public class AssetTransactionsAdapter extends BaseQuickAdapter<Transaction, Asse
     @Override
     protected void convert(ViewHolder viewHolder, Transaction transaction) {
         viewHolder.transactionTv.setText(transaction.getId());
+        boolean isSender=getAccount().getAddress().equals(transaction.getSenderId());
        if ( TransactionType.Transfer.getCode()==transaction.getType()){
-           viewHolder.amountTv.setText(String.format("%.3f",transaction.getAmount()/ (double)AschConst.COIN)+" XAS");
+           viewHolder.amountTv.setText(String.format("%s%.3f", isSender?"-":"+",transaction.getAmount()/ (double)AschConst.COIN)+" XAS");
        }else if (TransactionType.UIATransfer.getCode()==transaction.getType()){
            UIATransferAsset asset=(UIATransferAsset)transaction.getAssetInfo();
-           viewHolder.amountTv.setText(String.format("%.3f",Float.parseFloat(asset.getUiaTransfer().getAmountShow()))+" "+asset.getUiaTransfer().getCurrency());
+           viewHolder.amountTv.setText(String.format("%s%.3f", isSender?"-":"+",Float.parseFloat(asset.getUiaTransfer().getAmountShow()))+" "+asset.getUiaTransfer().getCurrency());
 
 //           viewHolder.amountTv.setText(String.format("%.3f",transaction.getAmount()/ (double)AschConst.COIN)+" "+asset.getUiaTransfer().getCurrency());
        }
 
+    }
+
+    private Account getAccount(){
+        return AccountsManager.getInstance().getCurrentAccount();
     }
 
     static class ViewHolder extends BaseViewHolder {
