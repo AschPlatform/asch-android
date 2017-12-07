3e290566be940bb0aab1275d2e4ae556c06a431e
kimziv <kimziv@qq.com>
2017-12-07 17:51:25 +0800

    change the default node url

--- app/src/main/java/asch/so/wallet/AppConstants.java
+++ app/src/main/java/asch/so/wallet/AppConstants.java
@@ -12,8 +12,8 @@ public class AppConstants {
    // public static final  String DEFAULT_NODE_URL="http://101.200.84.232:4097";//"http://192.168.2.124:4096";//
     //public static final  String DEFAULT_NODE_URL="http://192.168.2.124:4096";//
     //public static final String DEFAULT_NODE_URL ="http://192.168.1.101:4096";
-   //public static final  String DEFAULT_NODE_URL="http://47.93.137.170:4097";
-    public static final  String DEFAULT_NODE_URL="http://testnet.asch.io:4096";
+   public static final  String DEFAULT_NODE_URL="http://47.93.137.170:4097";
+    //public static final  String DEFAULT_NODE_URL="http://testnet.asch.io:4096";
    public static final  String DEFAULT_MAGIC="594fe0f3";
 
 //   public static final  String DEFAULT_NODE_URL="http://mainnet.asch.io";
